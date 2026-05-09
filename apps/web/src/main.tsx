4ff2a1cc052cddb647aacff667c263d5754173cb
Chii Yuen <chii.yuen@hotmail.com>
2026-05-10 00:12:25 +0800

    Add query client

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,15 +1,19 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@workspace/ui/globals.css";
 
 import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { queryClient } from "@/lib/query-client.ts";
 import { router } from "@/router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
